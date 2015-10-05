6966284116e8a2f1a6e13ea6713cf29d1f0e90c1
rickyrug <rickyrug@air-de-ricardo.lan>
2015-10-04 21:26:54 -0500

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- application/views/header.php
+++ application/views/header.php
@@ -17,10 +17,10 @@ and open the template in the editor.
             echo link_tag('bootstrap/css/mystyle.css');
         ?>
         <script src="<?php echo base_url().'js/jquery/jquery.js'; ?>"></script>
+        <script src="<?php echo base_url().'js/jqueryui/jquery-ui.js'; ?>"></script>
         <script src="<?php echo base_url().'js/jquery/jquery.tablesorter.min.js'; ?>"></script>
         <script src="<?php echo base_url().'js/jquery/jquery.tablesorter.widgets.min.js'; ?>"></script>
-        <script src="<?php echo base_url().'js/listfunctions.js'; ?>"></script>
-        <script src="<?php echo base_url().'js/jqueryui/jquery-ui.js'; ?>"></script>
+        <script src="<?php echo base_url().'js/listfunctions.js'; ?>"></script>       
         <script src="<?php echo base_url().'js/datepicker.js'; ?>"></script>
     </head>
     <body>
--- js/datepicker.js
+++ js/datepicker.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
-     $('#fechafin').datepicker();
+ $(function () {
+        $("#fechaini").datepicker;
+    });
 
-});
