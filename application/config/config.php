2467a9006cc99300557a02d31782f84bee88cd57
rickyrug <rickyrug@air-de-ricardo.lan>
2016-10-27 21:47:39 -0500

    inicio

--- application/config/config.php
+++ application/config/config.php
@@ -17,8 +17,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://stocktracker.comercialdistribuidora.com';
-
+/*$config['base_url'] = 'http://stocktracker.comercialdistribuidora.com';*/
+$config['base_url'] = 'http://localhost/stocktraking';
 /*
 |--------------------------------------------------------------------------
 | Index File
